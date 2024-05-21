312840b674d46b79e6636003f63fb851a39e18d7
gpientka <grit.pientka@t-systems.com>
2024-05-21 11:12:36 +0200

    added computation of performance metrics on slices of categorical features

--- ml/model.py
+++ ml/model.py
@@ -1,6 +1,8 @@
 from sklearn.metrics import fbeta_score, precision_score, recall_score
 from sklearn.ensemble import HistGradientBoostingClassifier
 
+import pickle
+import pandas as pd
 
 # Optional: implement hyperparameter tuning.
 def train_model(X_train, y_train):
@@ -49,11 +51,12 @@ def compute_model_metrics(y, preds):
 
 
 def inference(model, X):
-    """ Run model inferences and return the predictions.
+    """ 
+    Runs model inferences and returns the predictions.
 
     Inputs
     ------
-    model : ???
+    model : sklearn object
         Trained machine learning model.
     X : np.array
         Data used for prediction.
@@ -64,3 +67,54 @@ def inference(model, X):
     """
     y_pred = model.predict(X)
     return y_pred
+
+def compute_slice_metrics(df, feature, y_test, y_pred):
+    """
+    Computes performance metrics on slices for a given categorical feature.
+    A slice corresponds to one value of the categorical feature analyzed.
+    
+    Inputs
+    ------
+    df: pandas dataframe
+        Test dataframe obtained from train-test split.
+    feature: string
+        Feature to analyze.
+    y_test : np.array
+        Known labels of test dataset.
+    y_pred : np.array
+        Predicted labels for test dataset.
+
+    Returns
+    ------
+    metrics_df: pandas dataframe with
+        n_samples: integer - number of data samples in the slice
+        precision : float
+        recall : float
+        f1 : float
+    Each row corresponds one unique value of the feature.
+    """    
+    slice_values = df[feature].unique().tolist()
+    metrics_df = pd.DataFrame(index=slice_values, columns=['feature','n_samples','precision', 'recall', 'f1'])
+
+    for val in slice_values:
+        slice_mask = df[feature]==val
+
+        slice_y = y_test[slice_mask]
+        slice_preds = y_pred[slice_mask]
+        precision, recall, fbeta = compute_model_metrics(slice_y, slice_preds)
+        
+        metrics_df.at[val, 'feature'] = feature
+        metrics_df.at[val, 'n_samples'] = len(slice_y)
+        metrics_df.at[val, 'precision'] = precision
+        metrics_df.at[val, 'recall'] = recall
+        metrics_df.at[val, 'f1'] = fbeta
+
+    # reorder first two columns to have feature before feature value
+    metrics_df.reset_index(names='feature value', inplace=True)
+    colList = list(metrics_df.columns)
+    colList[0], colList[1] =  colList[1], colList[0]
+    metrics_df = metrics_df[colList]
+    
+    #print(metrics_df)
+
+    return metrics_df
--- ml/train_model.py
+++ ml/train_model.py
@@ -3,50 +3,75 @@
 from sklearn.model_selection import train_test_split
 
 # Add the necessary imports for the starter code.
+import os
 import pandas as pd
 import pickle
 from data import process_data
-from model import train_model, inference, compute_model_metrics
+from model import train_model, inference, compute_model_metrics, compute_slice_metrics
 
-# Add code to load in the data.
-data = pd.read_csv("../data/census.csv")
 
-# Optional enhancement, use K-fold cross validation instead of a train-test
-# split.
-train, test = train_test_split(data, random_state=42, test_size=0.20,
+if __name__=="__main__":
+
+    # Add code to load in the data.
+    data = pd.read_csv("../data/census.csv")
+
+    # Optional enhancement, use K-fold cross validation instead of a train-test
+    # split.
+    train, test = train_test_split(data, random_state=42, test_size=0.20,
                                stratify=data['salary'])
 
-cat_features = [
-    "workclass",
-    "education",
-    "marital-status",
-    "occupation",
-    "relationship",
-    "race",
-    "sex",
-    "native-country",
-]
-X_train, y_train, encoder, lb = process_data(
-    train, categorical_features=cat_features, label="salary", training=True
-)
+    cat_features = [
+        "workclass",
+        "education",
+        "marital-status",
+        "occupation",
+        "relationship",
+        "race",
+        "sex",
+        "native-country",
+    ]
+    X_train, y_train, encoder, lb = process_data(
+        train, categorical_features=cat_features, label="salary", training=True
+    )
 
-# Proces the test data with the process_data function.
-X_test, y_test, encoder, lb = process_data(
-    test, categorical_features=cat_features, label="salary", training=False,
-    encoder=encoder, lb=lb
-)
+    # Proces the test data with the process_data function.
+    X_test, y_test, encoder, lb = process_data(
+        test, categorical_features=cat_features, label="salary", training=False,
+        encoder=encoder, lb=lb
+    )
 
-# Train and save a model.
-model = train_model(X_train, y_train)
+    # Train and save a model.
+    model = train_model(X_train, y_train)
+    # Save artifacts
+    pickle.dump(model, open("../model/model.pkl", "wb"))
+    pickle.dump(encoder, open("../model/encoder.pkl", 'wb'))
+    pickle.dump(lb, open("../model/lb.pkl", 'wb'))
 
-# Scoring
-y_pred = inference(model, X_test)
-precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
-print("precision:", precision)
-print("recall:", recall)
-print("fbeta:", fbeta)
 
-# Save artifacts
-pickle.dump(model, open("../model/model.pkl", "wb"))
-pickle.dump(encoder, open("../model/encoder.pkl", 'wb'))
-pickle.dump(lb, open("../model/lb.pkl", 'wb'))
+    # Scoring
+    y_pred = inference(model, X_test)
+    precision, recall, fbeta = compute_model_metrics(y_test, y_pred)
+    print("precision:", precision)
+    print("recall:", recall)
+    print("fbeta:", fbeta)
+
+    # on individual slices
+    #with open('./slice_output.txt', 'w') as f:
+    #    for feat in cat_features:
+    #        f.write("Investigating performance for {} categories:\n".format(feat))
+    #        slice_metrics = compute_slice_metrics(test, feat, y_test, y_pred)
+    #        f.write(slice_metrics)
+
+    if os.path.exists('./slice_output.csv'):
+        os.remove('./slice_output.csv')
+
+    metrics_collection = pd.DataFrame(columns=['feature', 'feature value', 'n_samples', 'precision', 'recall', 'f1'])
+    for feat in cat_features:
+        slice_metrics = compute_slice_metrics(test, feat, y_test, y_pred)
+        metrics_collection = pd.concat([metrics_collection, slice_metrics], axis=0)
+
+    metrics_collection.to_csv('./slice_output.csv', index=False)
+
+
+
+
